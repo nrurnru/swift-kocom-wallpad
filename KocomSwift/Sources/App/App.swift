1f265adb29629130fca0dcd36365c59bf7be72c7
nrurnru <nrurnru@gmail.com>
2025-03-08 11:16:46 +0900

    chore: 컨벤션 수정

--- KocomSwift/Sources/App/App.swift
+++ KocomSwift/Sources/App/App.swift
@@ -10,24 +10,24 @@ import Foundation
 final class App {
     private var rs485Service: RS485Service!
     private var mqttService: MQTTService!
-    private var HomeAssistantService: HomeAssistantService!
+    private var homeAssistantService: HomeAssistantService!
     
     private init() { }
     static let shared = App()
 
     func initialize() throws {
         self.rs485Service = try RS485Service.initialize()
-        self.HomeAssistantService = HomeAssistantService()
+        self.homeAssistantService = HomeAssistantService()
         
         let mqttService = try MQTTService(
             rs485Service: self.rs485Service,
-            discovery: self.HomeAssistantService
+            discovery: self.homeAssistantService
         )
         
         self.mqttService = mqttService
         
-        self.HomeAssistantService.setMQTTService(mqttService: mqttService)
-        self.rs485Service.setHomeassistantService(self.HomeAssistantService)
+        self.homeAssistantService.setMQTTService(mqttService: mqttService)
+        self.rs485Service.setHomeassistantService(self.homeAssistantService)
     }
     
     func start() throws {
